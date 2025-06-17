96e49ea9dfb24d2daa30e1d21c13e63e4409be22
MajorTom <invaderzgame@gmail.com>
2025-06-18 00:06:58 +0700

    FEAT: add next token functionality

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -17,7 +17,43 @@ func New(input string) *Lexer {
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+
+	}
+
+	l.readChar()
 	return tok
+}
+
+// func (l *Lexer) readIdentifier() string {
+
+// }
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{
+		Type:    tokenType,
+		Literal: string(ch),
+	}
 }
 
 func (l *Lexer) readChar() {
